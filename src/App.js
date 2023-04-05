7f9242bdcd4e2cac84e69f564d32d5b0fa61c80e
anthony-melsoner_wwg <anthony.melsoner@grainger.com>
2023-04-05 10:48:30 -0400

    todo app started

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
+import Todo from "./components/Todo";
 
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <div className='card'>
-        <h2>TITLE</h2>
-        <div className="actions">
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+      <h1> My Todos</h1>
+      <Todo text='1' />
+      <Todo text='2' />
+      <Todo text='3' />
     </div>
   );
 }
